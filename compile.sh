484942276ff718e8d3d0c748b3bfc60f8dd35658
julisaverdejo <julisa.verdejo@gmail.com>
2023-10-11 10:43:36 -0600

    Plantilla de tesis terminada

---
+++ compile.sh
@@ -0,0 +1,19 @@
+echo "--------------------"
+echo "COMPILACIÓN DE TESIS"
+echo "--------------------"
+echo "1. Iniciando . . ."
+latex main.tex > logs.txt
+echo "2. Primera compilacion terminada . . ."
+bibtex main >> logs.txt
+echo "3. BibTeX compilado correctamente . . ."
+#makeglossaries main >> logs.txt
+#echo "4. Glosarios compilado correctamente  . . ."
+latex main.tex >> logs.txt
+echo "5. Segunda compilacion terminada . . ."
+pdflatex main.tex >> logs.txt
+echo "6. Generando PDF . . ."
+rm *.aux *.toc *.lol *.lot *.lof *.ist *.glo *.glg *.gls *.log *.bbl *.out *.dvi *.blg >> logs.txt
+echo "7. Eliminando archivos temporales . . ."
+echo "8. Proceso terminado . . ."
+# read -p "7. Presiona [Enter] para salir . . ."
+# sleep 0.5
